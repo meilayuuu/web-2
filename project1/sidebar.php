b6af7427460591559739c199efd2ab7969be4aff
meilayuuu <0110124051@student.nurulfikri.ac.id>
2025-05-15 07:50:05 +0700

    up project01

---
+++ project1/sidebar.php
@@ -0,0 +1,34 @@
+<div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                            <div class="nav">
+                            <div class="sb-sidenav-menu-heading">
+                            <img src="assets/img/mei3.jpg" alt="Profile Photo" class="img-fluid rounded-circle me-2" style="width: 35px; height: 35px;">
+                            <span class="text-white" style="font-size: 14px;">Meila Ayu</span>
+                            </div>
+                            <div class="sb-sidenav-menu-heading">Core</div>
+                            <a class="nav-link" href="index.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Dashboard
+                            </a>
+                            
+
+
+                            <li><a class="nav-link" href="prodi.php"><i class="fa-solid fa-building-columns me-2"></i> Prodi</a></li>
+                            <li><a class="nav-link" href="dosen.php"><i class="fa-solid fa-user-tie me-2"></i> Dosen</a></li>
+                            <li><a class="nav-link" href="kegiatan.php"><i class="fa-solid fa-calendar-days me-2"></i> Kegiatan</a></li>
+                            <li><a class="nav-link" href="jenis_kegiatan.php"><i class="fa-solid fa-layer-group me-2"></i> Jenis Kegiatan</a></li>
+                            <li><a class="nav-link" href="dosen_kegiatan.php"><i class="fa-solid fa-user-clock me-2"></i> Dosen Kegiatan</a></li>
+                            <li><a class="nav-link" href="penelitian.php"><i class="fa-solid fa-book-open-reader me-2"></i> Penelitian</a></li>
+                            <li><a class="nav-link" href="tim_penelitian.php"><i class="fa-solid fa-people-group me-2"></i> Tim Penelitian</a></li>
+                            <li><a class="nav-link" href="bidang_ilmu.php"><i class="fa-solid fa-flask-vial me-2"></i> Bidang Ilmu</a></li>
+
+
+                            
+                            
+                            
+                    </div>
+
+                </nav>
+            </div>
